b26f37ce5bf180c8dd5e5b1e4a636ce78d4ea640
Fernando Oliveira <fernandoaju78@gmail.com>
2024-03-03 21:55:30 -0300

    partition: Exclude ksu modules file from userdata backup
    Change-Id: I4b42f52ed9ef00e0c9c39b1de80bf47a2ade65cd
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -1299,6 +1299,7 @@ void TWPartition::Setup_Data_Media() {
 		backup_exclusions.add_absolute_dir("/data/fonts");
 		backup_exclusions.add_absolute_dir("/data/nandswap");
 		backup_exclusions.add_absolute_dir("/data/gsi"); // Contains huge files (DSU System image + Userdata image), and won't work after restoration (requires configuration files in metadata)
+		backup_exclusions.add_absolute_dir("/data/adb/ksu/modules.img"); //After ksu 0.8.x the modules.img file became 1tb, which is inhibiting the execution of backups
 		wipe_exclusions.add_absolute_dir(Mount_Point + "/misc/vold"); // adopted storage keys
 		ExcludeAll(Mount_Point + "/system/storage.xml");
 
