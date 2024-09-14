d7bae93bf0d0a4234a296f8ebd86a0388f6a2d9d
nebrassy <nebras30@gmail.com>
2024-09-14 19:08:06 +0200

    Revert "twrp: use custom custom fscrypt library [1/2]"
    This reverts commit f15d73844ea7a72c79a7055491975712a8140534.
    
    Change-Id: Ibc8b100dd924e6548e49cd20bc7ad1d54903473d

--- libtar/Android.mk
+++ libtar/Android.mk
@@ -40,7 +40,7 @@ ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
         libkeystore-attestation-application-id \
         libhardware \
         libprotobuf-cpp-lite \
-        libfscrypttwrp \
+        libfscrypt \
         libhidlbase \
         libutils \
         libbinder \
